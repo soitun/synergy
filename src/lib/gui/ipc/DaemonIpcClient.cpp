6337d1768f9c21ae0a467988df9a18c5af9b2279
Nick Bolton <nick@symless.com>
2025-04-21 14:14:19 +0100

    Daemon IPC client connect retry (#33)
    * refactor(gui): Consolidate IPC connection state to one member
    
    * Make daemon IPC log messages more consistent
    
    * Retry mechanism for IPC client connect
    
    * Only return true for IPC connect if successful
    
    * Always disconnect on error and improve signal name

--- src/lib/gui/ipc/DaemonIpcClient.cpp
+++ src/lib/gui/ipc/DaemonIpcClient.cpp
@@ -15,9 +15,8 @@
 
 namespace deskflow::gui::ipc {
 
-// At 1 second, we seem to be timing out on the first connection attempt, even though the server is
-// listening and responding. Let's try 2 seconds and see if that makes a difference.
-const auto kTimeout = 2000;
+const auto kTimeout = 1000;
+const auto kRetryLimit = 3;
 
 DaemonIpcClient::DaemonIpcClient(QObject *parent)
     : QObject(parent),
@@ -29,60 +28,95 @@ DaemonIpcClient::DaemonIpcClient(QObject *parent)
 
 bool DaemonIpcClient::connectToServer()
 {
-  if (m_connected) {
-    qDebug() << "daemon ipc client already connected to server";
-    return true;
-  }
-
-  if (m_connecting) {
+  if (m_state == State::Connecting) {
     qWarning() << "daemon ipc client already connecting to server";
+    return false;
+  }
+
+  if (m_state != State::Unconnected) {
+    qDebug() << "daemon ipc client not in unconnected state, disconnecting";
+    disconnectFromServer();
+  }
+
+  if (m_socket->state() != QLocalSocket::UnconnectedState) {
+    qWarning() << "daemon ipc client socket not in unconnected state, disconnecting";
+    disconnectFromServer();
+  }
+
+  for (int i = 0; i < kRetryLimit; ++i) {
+    if (i == 0) {
+      qDebug() << "daemon ipc client connecting to server:" << kDaemonIpcName;
+    } else {
+      qDebug() << "daemon ipc client retrying connection, attempt:" << i + 1;
+    }
+
+    m_state = State::Connecting;
+    m_socket->connectToServer(kDaemonIpcName);
+
+    if (!m_socket->waitForConnected(kTimeout)) {
+      qWarning() << "daemon ipc client failed to connect";
+      disconnectFromServer();
+      continue;
+    }
+
+    if (!sendMessage("hello", "hello", false)) {
+      qWarning() << "daemon ipc client failed to send hello";
+      disconnectFromServer();
+      continue;
+    }
+
+    m_state = State::Connected;
+    qDebug() << "daemon ipc client connected";
+    Q_EMIT connected();
     return true;
   }
 
-  qDebug() << "daemon ipc client connecting to server:" << kDaemonIpcName;
-  m_connecting = true;
-  m_socket->connectToServer(kDaemonIpcName);
+  qWarning() << "daemon ipc client failed to connect after" << kRetryLimit << "attempts";
+  disconnectFromServer();
+  Q_EMIT connectionFailed();
+  return false;
+}
 
-  if (!m_socket->waitForConnected(kTimeout)) {
-    qWarning() << "ipc client failed to connect to server:" << kDaemonIpcName;
-    m_connecting = false;
-    Q_EMIT connectFailed();
-    return false;
+void DaemonIpcClient::disconnectFromServer()
+{
+  m_state = State::Disconnecting;
+  qDebug() << "daemon ipc client disconnecting from server";
+  m_socket->disconnectFromServer();
+
+  if (m_socket->state() != QLocalSocket::UnconnectedState) {
+    qDebug() << "daemon ipc client waiting for socket to disconnect";
+    m_socket->waitForDisconnected(kTimeout);
+    qDebug() << "daemon ipc client disconnected from server";
+  } else {
+    qDebug() << "daemon ipc client socket already disconnected";
   }
 
-  if (!sendMessage("hello", "hello", false)) {
-    qWarning() << "ipc client failed to send hello";
-    m_connecting = false;
-    Q_EMIT connectFailed();
-    return false;
-  }
-
-  m_connecting = false;
-  m_connected = true;
-
-  qDebug() << "ipc client connected to server:" << kDaemonIpcName;
-  Q_EMIT connected();
-
-  return true;
+  m_state = State::Unconnected;
 }
 
 void DaemonIpcClient::handleDisconnected()
 {
-  qWarning() << "ipc client disconnected from server";
-  m_connected = false;
-  Q_EMIT connectFailed();
+  qDebug() << "daemon ipc client disconnected from server";
+  if (m_state == State::Connected) {
+    Q_EMIT connectionFailed();
+  }
+
+  m_state = State::Unconnected;
 }
 
 void DaemonIpcClient::handleErrorOccurred()
 {
-  qWarning() << "ipc client error:" << m_socket->errorString();
-  m_connected = false;
-  Q_EMIT connectFailed();
+  qWarning() << "daemon ipc client error:" << m_socket->errorString();
+  disconnectFromServer();
+
+  if (m_state == State::Connected) {
+    Q_EMIT connectionFailed();
+  }
 }
 
 bool DaemonIpcClient::sendMessage(const QString &message, const QString &expectAck, const bool expectConnected)
 {
-  if (expectConnected && !m_connected) {
+  if (expectConnected && !isConnected()) {
     qWarning() << "cannot send command, ipc client not connected";
     return false;
   }
@@ -90,12 +124,12 @@ bool DaemonIpcClient::sendMessage(const QString &message, const QString &expectA
   QByteArray messageData = message.toUtf8() + "\n";
   m_socket->write(messageData);
   if (!m_socket->waitForBytesWritten(kTimeout)) {
-    qWarning() << "ipc client failed to write command";
+    qWarning() << "daemon ipc client failed to write command";
     return false;
   }
 
   if (!expectAck.isEmpty()) {
-    qDebug() << "ipc client waiting for ack: " << expectAck;
+    qDebug() << "daemon ipc client waiting for ack: " << expectAck;
 
     if (!m_socket->waitForReadyRead(kTimeout)) {
       qWarning() << "daemon ipc client socket ready read timed out";
@@ -104,36 +138,36 @@ bool DaemonIpcClient::sendMessage(const QString &message, const QString &expectA
 
     QByteArray response = m_socket->readAll();
     if (response.isEmpty()) {
-      qWarning() << "ipc client got empty response";
+      qWarning() << "daemon ipc client got empty response";
       return false;
     }
 
     QString responseData = QString::fromUtf8(response);
     if (responseData.isEmpty()) {
-      qWarning() << "ipc client failed to convert response to string";
+      qWarning() << "daemon ipc client failed to convert response to string";
       return false;
     }
 
     if (responseData != expectAck + "\n") {
-      qWarning() << "ipc client got unexpected response: " << responseData;
+      qWarning() << "daemon ipc client got unexpected response: " << responseData;
       return false;
     }
   }
 
-  qDebug() << "ipc client sent message: " << messageData;
+  qDebug() << "daemon ipc client sent message: " << messageData;
   return true;
 }
 
 bool DaemonIpcClient::keepAlive()
 {
   if (!isConnected() && !connectToServer()) {
-    qWarning() << "ipc client keep alive failed to connect";
+    qWarning() << "daemon ipc client keep alive failed to connect";
     return false;
   }
 
   if (!sendMessage("noop")) {
-    qWarning() << "ipc client keep alive ping failed";
-    m_connected = false;
+    qWarning() << "daemon ipc client keep alive ping failed, reconnecting";
+    connectToServer();
     return false;
   }
 
@@ -180,31 +214,31 @@ QString DaemonIpcClient::requestLogPath()
   }
 
   if (!m_socket->waitForReadyRead(kTimeout)) {
-    qWarning() << "ipc client failed to read log path response";
+    qWarning() << "daemon ipc client failed to read log path response";
     return QString();
   }
 
   QByteArray response = m_socket->readAll();
   if (response.isEmpty()) {
-    qWarning() << "ipc client got empty log path response";
+    qWarning() << "daemon ipc client got empty log path response";
     return QString();
   }
 
   QString responseData = QString::fromUtf8(response);
   if (responseData.isEmpty()) {
-    qWarning() << "ipc client failed to convert log path response to string";
+    qWarning() << "daemon ipc client failed to convert log path response to string";
     return QString();
   }
 
   // Trimming removes newline from end of message.
   QStringList parts = responseData.trimmed().split("=");
   if (parts.size() != 2) {
-    qWarning() << "ipc client got invalid log path response: " << responseData;
+    qWarning() << "daemon ipc client got invalid log path response: " << responseData;
     return QString();
   }
 
   if (parts[0] != "logPath") {
-    qWarning() << "ipc client got unexpected log path response: " << responseData;
+    qWarning() << "daemon ipc client got unexpected log path response: " << responseData;
     return QString();
   }
 
