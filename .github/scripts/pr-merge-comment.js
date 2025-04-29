4482f79e8e77dd95e7969e959b87e3895eb89f8c
Nick Bolton <nick@symless.com>
2025-04-29 13:10:30 +0100

    Move logic to script from CI comment workflow (#38)
    * Refactor conditional logic into external script
    
    * Check out code before running script
    
    * Fixed references to GH objects
    
    * Improve job name
    
    * Fixed undef ref error
    
    * Add debug step (this is getting silly)
    
    * More debug (this is madness)
    
    * Use core outputs
    
    * Simplify workflow and restore original name
    
    * Fixed undefined refs
    
    * Echo github object
    
    * Reorder consts
    
    * Fixed input ref
    
    * Fixed var ref order
    
    * Fixed ref for repository.html_url
    
    * Add test values
    
    * Fixed test run ID

--- .github/scripts/pr-merge-comment.js
+++ .github/scripts/pr-merge-comment.js
@@ -1,26 +1,38 @@
-async function prMergeComment({github, context, version, sha, runId, runName, runResult, repoUrl}) {
+async function prMergeComment(github, context, version) {
+  if (!github) throw new Error("Arg `github` not defined.");
+  if (!context) throw new Error("Arg `context` not defined.");
 
-  if (!github) {
-    throw new Error('GitHub not defined.');
-  }
-
-  if (!context) {
-    throw new Error('Context not defined.');
-  }
+  console.log(context);
 
   if (!version) {
-    console.log('No version found, skipping.');
+    console.log("No version found, skipping.");
     return;
   }
 
   console.log(`Version: ${version}`);
 
+  const workflowRun = context.payload.workflow_run;
+  const sha = context.payload.inputs["test-sha"] || workflowRun?.head_sha;
   if (!sha) {
-    console.log('No Git SHA found, skipping.');
+    console.log("No Git SHA found, skipping.");
     return;
   }
 
   console.log(`SHA: ${sha}`);
+
+  const isMaster = workflowRun?.head_branch.startsWith("master");
+  const pushToMaster = workflowRun?.event === "push" && isMaster;
+
+  if (context.eventName !== "workflow_dispatch" && !pushToMaster) {
+    console.log("Skipping, not workflow dispatch or workflow run push to master.");
+    return;
+  }
+
+  // Either use the run info from the workflow run or the context payload (for testing).
+  const runId = workflowRun?.id || context.runId;
+  const runName = workflowRun?.name || "test";
+  const runResult = workflowRun?.conclusion || "test";
+  const repoUrl = context.payload.repository.html_url;
 
   const { data: pullRequests } = await github.rest.repos.listPullRequestsAssociatedWithCommit({
     owner: context.repo.owner,
@@ -29,21 +41,21 @@ async function prMergeComment({github, context, version, sha, runId, runName, ru
   });
 
   if (pullRequests.length < 1) {
-    console.log('No PR found, skipping.');
+    console.log("No PR found, skipping.");
     return;
   }
 
   console.log(`Found ${pullRequests.length} PR(s).`);
   const prNumber = pullRequests[0].number;
 
-  let body = 'Merge build complete.\n' + `Version: \`${version}\``;
+  let body = "Merge build complete.\n" + `Version: \`${version}\``;
 
   if (runId) {
     console.log(`Appending result and URL for run ID: ${runId}`);
     const runUrl = `${repoUrl}/actions/runs/${runId}`;
     body += `\nRun: [${runName}](${runUrl}) (${runResult})`;
   } else {
-    console.log('No run ID found, skipping run result and URL.');
+    console.log("No run ID found, skipping run result and URL.");
   }
 
   console.log(`Commenting on first PR: ${prNumber}`);
@@ -55,4 +67,6 @@ async function prMergeComment({github, context, version, sha, runId, runName, ru
   });
 }
 
-module.exports = prMergeComment;
+module.exports = {
+  prMergeComment,
+};
